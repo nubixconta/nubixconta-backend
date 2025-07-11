38b173d4a16adda644b70f9e5fbc7f362f428aef
el3azar <otoeleazarivas@gmail.com>
2025-07-11 00:20:28 -0600

    Funcionalidad y refactorizacion de Notas de Credito

--- src/main/java/com/nubixconta/modules/inventory/entity/Product.java
+++ src/main/java/com/nubixconta/modules/inventory/entity/Product.java
@@ -2,7 +2,6 @@ package com.nubixconta.modules.inventory.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
